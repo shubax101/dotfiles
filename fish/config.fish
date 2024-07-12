0afb66eab00900ad285a50588d1962c31610990d
shubax101 <shubax101@gmail.com>
2024-07-12 08:50:13 +0530

    better waybar

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
 end
 
+# set -x XDG_DATA_DIRS "/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
+
 starship init fish | source
---
+++ hypr/scripts/screenshot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+hyprshot -m region -z
