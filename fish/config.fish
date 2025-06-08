7e0760323f4d9fde22f0d5aed3fc32c6251373df
shubax101 <shubax101@gmail.com>
2025-06-08 11:32:15 +0530

    updated config

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
 end
 
+set -Ux fish_user_paths $HOME/bin $fish_user_paths
 # set -x XDG_DATA_DIRS "/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
 
 starship init fish | source
